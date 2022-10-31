15fe7639d4e839da09b01e978dd306e330f50ab5
Kiera Saltz <hello@kierasaltz.com>
2022-10-31 16:25:12 -0400

    Update

--- setup/pkgroot/usr/local/outset/boot-every/update.sh
+++ setup/pkgroot/usr/local/outset/boot-every/update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd "/Library/Application Support/Tech Suite Deploy/techsuite-deploy" || exit 1
+cd "/Shared/.ts-deploy/techsuite-deploy" || exit 1
 if [[ `git status --porcelain` ]]; then
   git pull
   cd setup || exit 1
   make pkg
   installer -pkg techsuite-deploy-*.pkg -target /
-  profiles -I -F "/Library/Application Support/Tech Suite Deploy/techsuite-deploy/Tech Suite.mobileconfig"
 fi
