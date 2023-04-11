78a7c942cb58ed3d0ac6f5e6099d3032da05cbe7
Kiera Saltz <hello@kierasaltz.com>
2023-04-11 11:44:57 -0400

    Fix ableton licensing issues

--- all/pkgroot/Library/Admin Tools/ableton-reset-prefs.sh
+++ all/pkgroot/Library/Admin Tools/ableton-reset-prefs.sh
@@ -3,4 +3,4 @@
 read -p "This will forcefully close Ableton if its open. Press [enter] to continue."
 echo 
 killall -q Ableton Live
-sudo rm -rf $HOME/Library/Preferences/Ableton
+rm -rf $HOME/Library/Preferences/Ableton
---
+++ all/pkgroot/usr/local/outset/login-every/reset-ableton-prefs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+killall -q Ableton Live
+rm -rf $HOME/Library/Preferences/Ableton
