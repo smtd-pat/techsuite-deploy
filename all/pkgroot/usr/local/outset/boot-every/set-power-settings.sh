383bf741ed782a33404ce9a5abf0a9b82aff9809
Kiera Saltz <hello@kierasaltz.com>
2023-12-12 13:58:08 -0500

    Restoring non-profile based power settings for now

---
+++ all/pkgroot/usr/local/outset/boot-every/set-power-settings.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/usr/sbin/systemsetup -setsleep 90
+/usr/sbin/systemsetup -setcomputersleep 105
+/usr/sbin/systemsetup -setwakeonnetworkaccess on
+/usr/sbin/systemsetup -setrestartpowerfailure on
+/usr/bin/pmset -c disksleep 1
+/usr/bin/pmset -c darkwakes 1
+/usr/bin/pmset repeat restart weekdays 07:00:00
